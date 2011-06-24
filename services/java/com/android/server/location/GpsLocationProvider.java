fa3497285ae8be3650dd18baa11975be714d6f94
Kevin Tang <zhikait@codeaurora.org>
2011-06-23 17:32:18 -0700

    GPS provider disable XTRA download on powerup
    changed GpsLocationProvider and disable the auto XTRA data download at powerup.
    
    Change-Id: I3a2274a10064c57f94718b2906314b2e8a8e8eb2

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -201,7 +201,7 @@ public class GpsLocationProvider implements LocationProviderInterface {
     // flags to trigger NTP or XTRA data download when network becomes available
     // initialized to true so we do NTP and XTRA when the network comes up after booting
     private boolean mInjectNtpTimePending = true;
-    private boolean mDownloadXtraDataPending = true;
+    private boolean mDownloadXtraDataPending = false;
 
     // set to true if the GPS engine does not do on-demand NTP time requests
     private boolean mPeriodicTimeInjection;

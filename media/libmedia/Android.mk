12705b76226f8cadc76e48ec0593b1126728732c
Ricardo Cerqueira <github@cerqueira.org>
2011-11-16 18:40:30 +0000

    audio: Add compatibility bits to use legacy libaudio implementations
    Enable with BOARD_USES_AUDIO_LEGACY, requires the use of the generic
    legacy audio shim
    
    Conflicts:
    
    	media/libmedia/Android.mk
    
    Change-Id: Ifbd485c8c5c976d9fd8d2c73cd32fd5010deb961

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -57,16 +57,16 @@ ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
         AudioParameter.cpp
 
     LOCAL_CFLAGS += -DUSES_AUDIO_LEGACY
-endif
-
-ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
-    LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
+    ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
+        LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
+    endif
 endif
 
 LOCAL_SHARED_LIBRARIES := \
     libui libcutils libutils libbinder libsonivox libicuuc libexpat \
     libcamera_client libstagefright_foundation \
     libgui libdl
+
 
 LOCAL_WHOLE_STATIC_LIBRARY := libmedia_helper
 

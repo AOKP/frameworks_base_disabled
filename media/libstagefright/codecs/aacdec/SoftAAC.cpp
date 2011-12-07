4fded070d0f995ef8a9f99ed3160422dca0fd343
Avss Srikanth <srikan@codeaurora.org>
2011-12-07 05:23:23 -0800

    libstagefright: Increase input and output buffer size for sw aac component
    - Increase input and output buffer size for sw aac component.
    - This is required for playback of httplive streaming scenarios
      where an accessunit contains aac data larger than provided
      input buffer size.
    
    Change-Id: Ic461067cf7b81363e833e912dd0ddbec73188ec1

--- media/libstagefright/codecs/aacdec/SoftAAC.cpp
+++ media/libstagefright/codecs/aacdec/SoftAAC.cpp
@@ -69,7 +69,7 @@ void SoftAAC::initPorts() {
     def.eDir = OMX_DirInput;
     def.nBufferCountMin = kNumBuffers;
     def.nBufferCountActual = def.nBufferCountMin;
-    def.nBufferSize = 8192;
+    def.nBufferSize = 8192*2;
     def.bEnabled = OMX_TRUE;
     def.bPopulated = OMX_FALSE;
     def.eDomain = OMX_PortDomainAudio;
@@ -87,7 +87,7 @@ void SoftAAC::initPorts() {
     def.eDir = OMX_DirOutput;
     def.nBufferCountMin = kNumBuffers;
     def.nBufferCountActual = def.nBufferCountMin;
-    def.nBufferSize = 8192;
+    def.nBufferSize = 8192*2;
     def.bEnabled = OMX_TRUE;
     def.bPopulated = OMX_FALSE;
     def.eDomain = OMX_PortDomainAudio;

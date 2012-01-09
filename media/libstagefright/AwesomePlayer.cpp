10777ca06777bf448f2580dd94cb68dcdf44e611
Phani Kumar Allada <pallad@codeaurora.org>
2012-01-09 17:04:27 +0530

    frameworks/base: Fix for video playback failure in browser
    - Issue: After camping the Signal to 3G network, open the browser
      and go to www.youtube.com and play any video. Video playback is
      failing after trying to load the video for few seconds. Browser is
      coming back to video's list.
    
    - Cause: At the beginning of playback, as there was a seek request
      pending, seek is done before starting the AudioPlayer. After
      AudioSource seeking, AudioPlayer start is called with OMX in PAUSED
      state, causing assert check fail for OMX state.
    
    - Fix: Call AudioSource start before calling AudioPlayer start, so that
      OMX moves to EXECUTING state.
    
    Change-Id: I4e746bf0b5167d4998fd900bbce3966a2dc1273d
    CRs-Fixed: 327929

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -1572,6 +1573,7 @@ void AwesomePlayer::finishSeekIfNecessary(int64_t videoTimeUs) {
 
 void AwesomePlayer::onVideoEvent() {
     Mutex::Autolock autoLock(mLock);
+    int mAudioSourcePaused = false;
     if (!mVideoEventPending) {
         // The event has been cancelled in reset_l() but had already
         // been scheduled for execution at that time.
@@ -1601,6 +1603,7 @@ void AwesomePlayer::onVideoEvent() {
                 modifyFlags(AUDIO_RUNNING, CLEAR);
             }
             mAudioSource->pause();
+            mAudioSourcePaused = true;
         }
     }
 
@@ -1642,6 +1645,10 @@ void AwesomePlayer::onVideoEvent() {
                 }
                 finishSeekIfNecessary(-1);
 
+                if (mAudioSourcePaused) {
+                    mAudioSource->start();
+                    mAudioSourcePaused = false;
+                }
                 if (mAudioPlayer != NULL
                         && !(mFlags & (AUDIO_RUNNING | SEEK_PREVIEW))) {
                     startAudioPlayer_l();
@@ -1690,6 +1697,10 @@ void AwesomePlayer::onVideoEvent() {
     SeekType wasSeeking = mSeeking;
     finishSeekIfNecessary(timeUs);
 
+    if (mAudioSourcePaused) {
+        mAudioSource->start();
+        mAudioSourcePaused = false;
+    }
     if (mAudioPlayer != NULL && !(mFlags & (AUDIO_RUNNING | SEEK_PREVIEW))) {
         status_t err = startAudioPlayer_l();
         if (err != OK) {

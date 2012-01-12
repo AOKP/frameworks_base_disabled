9aecab33ccefa1c49888de973ce6caf57ab86350
Martin Storsjo <martin@martin.st>
2012-01-12 14:25:29 +0200

    stagefright aacenc: Fix an inline asm constraint
    Output-only parameters should use =r or =&r constrains, never +r.
    
    This avoids a warning about the variable 'result' being used
    uninitialized.
    
    This avoids loading the uninitialized value into the register,
    before it is overwritten by the multiplication.
    
    Change-Id: I0ef6179e133c35d290feb8e12bea180ecae11a05
    
    Conflicts:
    
    	media/libstagefright/codecs/aacenc/basic_op/oper_32b.h

--- media/libstagefright/codecs/aacenc/basic_op/oper_32b.h
+++ media/libstagefright/codecs/aacenc/basic_op/oper_32b.h
@@ -60,10 +60,10 @@ __inline Word32 L_mpy_ls(Word32 L_var2, Word16 var1)
 __inline Word32 L_mpy_wx(Word32 L_var2, Word16 var1)
 {
 #if ARMV5TE_L_MPY_LS
-	Word32 result; 
-	asm volatile( 
-		"SMULWB  %[result], %[L_var2], %[var1] \n" 
-		:[result]"+r"(result)
+	Word32 result;
+	asm volatile(
+		"SMULWB  %[result], %[L_var2], %[var1] \n"
+		:[result]"=r"(result)
 		:[L_var2]"r"(L_var2), [var1]"r"(var1)
 		); 
 	return result;

39aadb8e88e4b7defd7943f398feb866f258b3ff
Prasanth Kamuju <kamuju@codeaurora.org>
2012-01-27 15:08:19 +0530

    Avoid video buffers filling and flushing in SEEK state.
       Added an extra check not to fill and flush the video buffers
       if the video buffer is NULL during video seek in PAUSE state.
    
    Change-Id: Id54115a16f96a650dea5ff5a45f6364b40e9ba34

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1323,7 +1323,7 @@ status_t AwesomePlayer::seekTo_l(int64_t timeUs) {
         notifyListener_l(MEDIA_SEEK_COMPLETE);
         mSeekNotificationSent = true;
 
-        if ((mFlags & PREPARED) && mVideoSource != NULL) {
+        if ((mFlags & PREPARED) && mVideoSource != NULL && mVideoBuffer!= NULL) {
             modifyFlags(SEEK_PREVIEW, SET);
             postVideoEvent_l();
         }

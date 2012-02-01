c923660f871f8f41925ec2081bc3ef2ad8d038ba
Jeykumar Sankaran <jsanka@codeaurora.org>
2012-02-01 11:58:14 -0800

    Support triple application buffers
    This change modifies surfaceflinger to use 3 application buffers
    by default for performance improvement.
    
    Change-Id: Ia3b1354f1b5d23f8237d73d1761436dd05ab004a

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -55,12 +55,16 @@ LOCAL_C_INCLUDES := \
 LOCAL_C_INCLUDES += hardware/libhardware/modules/gralloc
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifneq ($(BOARD_USES_LEGACY_QCOM),true)
+ifeq ($(TARGET_HAVE_BYPASS),true)
+    LOCAL_CFLAGS += -DBUFFER_COUNT_SERVER=3
+else
+    LOCAL_CFLAGS += -DBUFFER_COUNT_SERVER=2
+endif
+
 LOCAL_SHARED_LIBRARIES += \
 	libQcomUI
 LOCAL_C_INCLUDES += hardware/qcom/display/libqcomui
 LOCAL_CFLAGS += -DQCOM_HARDWARE
-endif
 ifeq ($(TARGET_QCOM_HDMI_OUT),true)
 LOCAL_CFLAGS += -DQCOM_HDMI_OUT
 endif

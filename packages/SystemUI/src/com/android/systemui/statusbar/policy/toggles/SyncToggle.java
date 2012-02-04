01575a36901816923c5ec477594b836b189761f8
Roman Birg <romanbirg@gmail.com>
2012-02-04 14:07:22 -0600

    SystemUI: keep sync toggle updated with correct state
    Fixes issue: http://code.google.com/p/aokp/issues/detail?id=217

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/SyncToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/SyncToggle.java
@@ -19,23 +19,14 @@ package com.android.systemui.statusbar.policy.toggles;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.database.ContentObserver;
-import android.net.ConnectivityManager;
-import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.IWindowManager;
+import android.content.SyncStatusObserver;
 
 import com.android.systemui.R;
 
 public class SyncToggle extends Toggle {
 
-    private ContentObserver mAccelerometerRotationObserver = new ContentObserver(new Handler()) {
-        @Override
-        public void onChange(boolean selfChange) {
+    private SyncStatusObserver mSyncObserver = new SyncStatusObserver() {
+        public void onStatusChanged(int which) {
             updateState();
         }
     };
@@ -43,36 +34,38 @@ public class SyncToggle extends Toggle {
     public SyncToggle(Context context) {
         super(context);
         updateState();
+        ContentResolver.addStatusChangeListener(ContentResolver.SYNC_OBSERVER_TYPE_SETTINGS,
+                mSyncObserver);
         setLabel(R.string.toggle_sync);
         if (mToggle.isChecked())
-        	setIcon(R.drawable.toggle_sync);
+            setIcon(R.drawable.toggle_sync);
         else
-        	setIcon(R.drawable.toggle_sync_off);
+            setIcon(R.drawable.toggle_sync_off);
     }
 
     @Override
     protected void onCheckChanged(boolean isChecked) {
         ContentResolver.setMasterSyncAutomatically(isChecked);
         if (isChecked)
-        	setIcon(R.drawable.toggle_sync);
+            setIcon(R.drawable.toggle_sync);
         else
-        	setIcon(R.drawable.toggle_sync_off);
+            setIcon(R.drawable.toggle_sync_off);
     }
 
     @Override
     protected void updateInternalToggleState() {
         mToggle.setChecked(ContentResolver.getMasterSyncAutomatically());
         if (mToggle.isChecked())
-        	setIcon(R.drawable.toggle_sync);
+            setIcon(R.drawable.toggle_sync);
         else
-        	setIcon(R.drawable.toggle_sync_off);
+            setIcon(R.drawable.toggle_sync_off);
     }
-    
+
     @Override
     protected boolean onLongPress() {
-    	Intent intent = new Intent(android.provider.Settings.ACTION_SYNC_SETTINGS);
+        Intent intent = new Intent(android.provider.Settings.ACTION_SYNC_SETTINGS);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         mContext.startActivity(intent);
-    	return true;
+        return true;
     }
 }

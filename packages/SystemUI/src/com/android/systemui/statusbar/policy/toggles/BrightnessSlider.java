e2b9028359b132c196af9e6ccffad738a7c8d5fe
Roman Birg <romanbirg@gmail.com>
2012-02-05 16:42:08 -0600

    SystemUI: make sure statusbar brightness slider updates properly

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/BrightnessSlider.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/toggles/BrightnessSlider.java
@@ -22,7 +22,9 @@ import com.android.systemui.statusbar.policy.ToggleSlider.Listener;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.database.ContentObserver;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IPowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -46,6 +48,8 @@ public class BrightnessSlider implements ToggleSlider.Listener {
     private ToggleSlider mControl;
     private IPowerManager mPower;
     private View mView;
+
+    boolean mSystemChange;
 
     public BrightnessSlider(Context context) {
         mContext = context;
@@ -86,6 +90,9 @@ public class BrightnessSlider implements ToggleSlider.Listener {
         mControl.setValue(value - mScreenBrightnessDim);
 
         mControl.setOnChangedListener(this);
+
+        SettingsObserver so = new SettingsObserver(new Handler());
+        so.observe();
     }
 
     public View getView() {
@@ -93,6 +100,9 @@ public class BrightnessSlider implements ToggleSlider.Listener {
     }
 
     public void onChanged(ToggleSlider view, boolean tracking, boolean automatic, int value) {
+        if (mSystemChange)
+            return;
+
         setMode(automatic ? Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC
                 : Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
         if (!automatic) {
@@ -118,6 +128,42 @@ public class BrightnessSlider implements ToggleSlider.Listener {
         try {
             mPower.setBacklightBrightness(brightness);
         } catch (RemoteException ex) {
+        }
+    }
+
+    private void updateValues() {
+        int automatic;
+        mSystemChange = true;
+        try {
+            automatic = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS_MODE);
+            mControl.setChecked(automatic != 0);
+
+            mControl.setValue(Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.SCREEN_BRIGHTNESS));
+
+        } catch (SettingNotFoundException e) {
+        }
+        mSystemChange = false;
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS), false,
+                    this);
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE), false, this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateValues();
         }
     }
 }

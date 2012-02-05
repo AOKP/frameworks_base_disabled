7e72360592a7fdca4d88fefeb4ccd809e40b1fd2
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-02-04 16:50:38 -0800

    adjust brightness with statusbar (Danesh)
    Conflicts:
    
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -42,8 +42,6 @@ import android.speech.tts.TextToSpeech;
 import android.text.TextUtils;
 import android.util.AndroidException;
 import android.util.Log;
-import android.view.WindowOrientationListener;
-
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -2454,6 +2452,13 @@ public final class Settings {
          * @hide
          */
         public static final String STATUSBAR_DATE_BEHAVIOR = "statusbar_date_behavior";
+
+        /**
+         * Whether to control brightness from status bar
+         * 
+         * @hide
+         */
+        public static final String STATUS_BAR_BRIGHTNESS_TOGGLE = "status_bar_brightness_toggle";
 
         /**
          * @hide

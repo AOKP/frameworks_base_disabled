e89ddcf5f73d8c689d48f923f21f7c69bf6741e0
Roman Birg <romanbirg@gmail.com>
2012-02-19 23:01:14 -0600

    SystemUI: add signal text (1/2) WIP
    Change-Id: I0b1a246f4744e71fb6cebfdfe2f674958971048e
    
    SystemUI: replace signal bars with signal text if option chosen
    
    Change-Id: I3b50747031f6e80edcad4499f0ad2646483c94c0
    
    fix airplane mode with SignalText
    Took the visibilty checks out of SignalText.java and moved to signalClusterView.
    Airplane mode icon now properly responds.
    
    Change-Id: I4e97854a3ed59df26d12b881b73b8f59c75de3db
    
    fix Dbm signal readout for LTE/4g
    This uses deprecated methods for getting signal values, but
    it is the way it's currently being done in Settings.apk in AOSP.
    there is no LTE functionality in the new preferred method.  Until
    such time as LTE signals are exposed, this looks to be the only way.
    
    Clean up
    I forgot to take out the log statements and delete
    commented out code.
    
    Change-Id: I96fe28fdf946e1372ec4dd8df3adae4aece4b509

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3006,6 +3006,27 @@ public final class Settings {
          * @hide
          */
         public static final String RECENT_KILL_ALL_BUTTON = "recent_kill_all_button";
+
+        /**
+         * Whether to show statusbar signal text
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_SIGNAL_TEXT = "statusbar_signal_text";
+
+        /**
+         * statusbar signal text color
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_SIGNAL_TEXT_COLOR = "statusbar_signal_text_color";
+
+        /**
+         * whether to hide the signal barss
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_HIDE_SIGNAL_BARS = "statusbar_hide_signal_bars";
     }
 
     /**
--- packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/SignalClusterView.java
@@ -16,7 +16,11 @@
 
 package com.android.systemui.statusbar;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Slog;
 import android.view.View;
@@ -24,10 +28,10 @@ import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
-import com.android.systemui.statusbar.policy.NetworkController;
+import android.widget.TextView;
 
 import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.NetworkController;
 
 // Intimately tied to the design of res/layout/signal_cluster_view.xml
 public class SignalClusterView
@@ -36,6 +40,8 @@ public class SignalClusterView
 
     static final boolean DEBUG = false;
     static final String TAG = "SignalClusterView";
+
+    private static final int EVENT_SIGNAL_STRENGTH_CHANGED = 200;
 
     NetworkController mNC;
 
@@ -47,9 +53,14 @@ public class SignalClusterView
     private int mAirplaneIconId = 0;
     private String mWifiDescription, mMobileDescription, mMobileTypeDescription;
 
+    private boolean showingSignalText = false;
+
     ViewGroup mWifiGroup, mMobileGroup;
     ImageView mWifi, mMobile, mWifiActivity, mMobileActivity, mMobileType, mAirplane;
+    TextView mMobileText;
     View mSpacer;
+
+    Handler mHandler;
 
     public SignalClusterView(Context context) {
         this(context, null);
@@ -79,8 +90,14 @@ public class SignalClusterView
         mMobile         = (ImageView) findViewById(R.id.mobile_signal);
         mMobileActivity = (ImageView) findViewById(R.id.mobile_inout);
         mMobileType     = (ImageView) findViewById(R.id.mobile_type);
+        mMobileText    = (TextView)  findViewById(R.id.signal_text);
         mSpacer         =             findViewById(R.id.spacer);
         mAirplane       = (ImageView) findViewById(R.id.airplane);
+
+        mHandler = new Handler();
+
+        SettingsObserver settingsObserver = new SettingsObserver(mHandler);
+        settingsObserver.observe();
 
         apply();
     }
@@ -94,6 +111,7 @@ public class SignalClusterView
         mMobile         = null;
         mMobileActivity = null;
         mMobileType     = null;
+        mMobileText     = null;
         mSpacer         = null;
         mAirplane       = null;
 
@@ -167,6 +185,13 @@ public class SignalClusterView
             mMobileActivity.setImageResource(mMobileActivityId);
             mMobileType.setImageResource(mMobileTypeId);
             mMobileGroup.setContentDescription(mMobileTypeDescription + " " + mMobileDescription);
+            if (showingSignalText && !mIsAirplaneMode) {
+                mMobile.setVisibility(View.GONE);
+                mMobileText.setVisibility(View.VISIBLE);
+            } else{
+                mMobile.setVisibility(View.VISIBLE);
+                mMobileText.setVisibility(View.GONE);
+            }
         } else {
             mMobileGroup.setVisibility(View.GONE);
         }
@@ -191,6 +216,33 @@ public class SignalClusterView
 
         mMobileType.setVisibility(
                 !mWifiVisible ? View.VISIBLE : View.GONE);
+    }
+
+    class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.STATUSBAR_SIGNAL_TEXT), false,
+                    this);
+            updateSettings();
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            updateSettings();
+        }
+    }
+
+    protected void updateSettings() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        showingSignalText = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_SIGNAL_TEXT, 0) != 0;
+        apply();
     }
 }
 

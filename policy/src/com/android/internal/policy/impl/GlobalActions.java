a4969c518a23583a061880099fe7e4060835a9ef
Roman Birg <romanbirg@gmail.com>
2012-02-20 23:15:44 -0600

    stop setting duplicate action

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -251,12 +251,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             void onToggle(boolean on) {
                 if (on) {
                     Intent i = new Intent(INTENT_TORCH_ON);
-                    i.setAction(INTENT_TORCH_ON);
                     i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mContext.startActivity(i);
                 } else {
                     Intent i = new Intent(INTENT_TORCH_OFF);
-                    i.setAction(INTENT_TORCH_OFF);
                     i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     mContext.startActivity(i);
                 }

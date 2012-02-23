09554e7aaa4e52d2fac54930e2620bbac66a3b1f
Al Sutton <al@funkyandroid.com>
2012-02-23 15:58:39 +0000

    Xcode 4.3 compatibility checkin
    The Xcode 4.3 compiler will only accept %z (lower case), and considers
    %Z to be an invalid format specifier.
    
    This patch adds a preprocessor conditional to provide a %z version of
    the formatting strings when the Xcode 4.3 compiler is used.
    
    Change-Id: I742a7832caa4b757240f206c1e09736bfc10d9e2
    Signed-off-by: Al Sutton <al@funkyandroid.com>

--- libs/rs/rsFifoSocket.cpp
+++ libs/rs/rsFifoSocket.cpp
@@ -70,9 +70,17 @@ size_t FifoSocket::read(void *data, size_t bytes) {
 }
 
 void FifoSocket::readReturn(const void *data, size_t bytes) {
+#if __APPLE_CC__ >= 5621
+    ALOGE("readReturn %p %zu", data, bytes);
+#else
     ALOGE("readReturn %p %Zu", data, bytes);
+#endif
     size_t ret = ::send(sv[1], data, bytes, 0);
+#if __APPLE_CC__ >= 5621
+    ALOGE("readReturn %zu", ret);
+#else
     ALOGE("readReturn %Zu", ret);
+#endif
     rsAssert(ret == bytes);
 }
 

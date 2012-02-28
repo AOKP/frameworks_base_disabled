3e6ce98587dc4040d2d5b6962ea84f9c2d9edb03
Jacob Bell <jacob@fzservers.com>
2012-02-27 22:33:42 -0500

    Patch from aeroevan

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -6051,8 +6051,10 @@ public class WindowManagerService extends IWindowManager.Stub
         int orientation = Configuration.ORIENTATION_SQUARE;
         if (dw < dh) {
             orientation = Configuration.ORIENTATION_PORTRAIT;
+            SystemProperties.set("sys.orientation.landscape", "0");
         } else if (dw > dh) {
             orientation = Configuration.ORIENTATION_LANDSCAPE;
+            SystemProperties.set("sys.orientation.landscape", "1");
         }
         config.orientation = orientation;
 

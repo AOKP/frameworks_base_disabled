c1c87c7a764dda4a9043c3eea097e9582be40b6a
Tejas Shikhare <tshikhar@codeaurora.org>
2012-02-29 20:01:39 -0800

    libstagefright: Fix for application not responding when going to next song
    - When a song is ended, the driver is paused, flushed and then close.
    - The issue is that under some scenario, the song is ended before the
      driver is initialized. Pausing the driver as result causes a native
      crash
    - Issue is fixed by only issuing the pause to the driver if it is
      started.
    
    Change-Id: Ib839a087136526e9186fc37c8cb29c681612e6c9
    CRs-Fixed: 339578
    (cherry picked from commit a7117328a21eca0fc422b56c12acfab25f17873a)
    
    Conflicts:
    
    	media/libstagefright/LPAPlayerALSA.cpp

--- media/libstagefright/LPAPlayerALSA.cpp
+++ media/libstagefright/LPAPlayerALSA.cpp
@@ -738,6 +738,15 @@ void LPAPlayer::reset() {
 
     pmemBufferDeAlloc();
     LOGE("Buffer Deallocation complete! Closing pcm handle");
+
+    if (local_handle->start) {
+        if (ioctl(local_handle->fd, SNDRV_PCM_IOCTL_PAUSE,1) < 0) {
+            LOGE("Audio Pause failed");
+        }
+    }
+    local_handle->start = 0;
+    if (mIsAudioRouted)
+       pcm_prepare(local_handle);
     pcm_close(local_handle);
     handle = (void*)local_handle;
 

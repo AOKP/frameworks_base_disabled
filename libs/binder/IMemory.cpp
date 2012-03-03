7239893017f448dbd8fa53a772d4e7523b9390a6
Laura Abbott <lauraa@codeaurora.org>
2012-03-04 00:36:00 +0100

    binder: add support for Ion
    Add classes to allocate from the Ion memory manager.

--- libs/binder/IMemory.cpp
+++ libs/binder/IMemory.cpp
@@ -109,6 +109,9 @@ private:
     mutable uint32_t    mFlags;
     mutable bool        mRealHeap;
     mutable Mutex       mLock;
+#ifdef QCOM_HARDWARE
+    mutable int         mIonFd;
+#endif
 };
 
 // ----------------------------------------------------------------------------
@@ -231,6 +234,9 @@ BpMemoryHeap::BpMemoryHeap(const sp<IBinder>& impl)
     : BpInterface<IMemoryHeap>(impl),
         mHeapId(-1), mBase(MAP_FAILED), mSize(0), mFlags(0), mRealHeap(false)
 {
+#ifdef QCOM_HARDWARE
+    mIonFd = open("/dev/ion", O_RDONLY);
+#endif
 }
 
 BpMemoryHeap::~BpMemoryHeap() {
@@ -257,6 +263,10 @@ BpMemoryHeap::~BpMemoryHeap() {
             free_heap(binder);
         }
     }
+#ifdef QCOM_HARDWARE
+    if (mIonFd > 0)
+        close(mIonFd);
+#endif
 }
 
 void BpMemoryHeap::assertMapped() const

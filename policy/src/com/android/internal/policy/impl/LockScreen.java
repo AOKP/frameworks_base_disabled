225bbbfad595eb715d44d4122b87b4a0644fecc1
Roman Birg <romanbirg@gmail.com>
2012-03-18 18:33:04 -0500

    add ability to set custom lockscreen target images

--- policy/src/com/android/internal/policy/impl/LockScreen.java
+++ policy/src/com/android/internal/policy/impl/LockScreen.java
@@ -16,30 +16,27 @@
 
 package com.android.internal.policy.impl;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-
 import android.app.ActivityManager;
-import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,6 +49,12 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.SlidingTab;
 import com.android.internal.widget.WaveView;
 import com.android.internal.widget.multiwaveview.MultiWaveView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 
 /**
@@ -339,7 +342,7 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
         String action = ACTION_NULL;
         Drawable icon;
         String customAppIntentUri;
-        int index;
+        final int index;
 
         public Target(int index) {
             this.index = index;
@@ -354,6 +357,26 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
             Drawable drawable = null;
             PackageManager pm = getContext().getPackageManager();
             Resources res = getContext().getResources();
+
+            String customIconUri = Settings.System.getString(getContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_CUSTOM_APP_ICONS[index]);
+
+            if (customIconUri != null && !customIconUri.equals("") && customIconUri.startsWith("file")) {
+                // it's an icon the user chose from the gallery here
+                File icon = new File(Uri.parse(customIconUri).getPath());
+                if(icon.exists())
+                    return resize(new BitmapDrawable(getResources(), icon.getAbsolutePath()));
+            } else if (customIconUri != null && !customIconUri.equals("")) {
+                // here they chose another app icon
+                try {
+                    return resize(pm.getActivityIcon(Intent.parseUri(customIconUri, 0)));
+                } catch (NameNotFoundException e) {
+                    e.printStackTrace();
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
+
             if (action.equals(ACTION_UNLOCK)) {
                 resId = R.drawable.ic_lockscreen_unlock;
                 drawable = res.getDrawable(resId);
@@ -425,7 +448,7 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
                 toastMessage(mCarrier, message, toastColor, toastIcon);
 
                 mCallback.pokeWakelock();
-            } else if (action.equals(ACTION_APP_CUSTOM)) {
+            } else if (action.equals(ACTION_APP_CUSTOM) && customAppIntentUri != null) {
                 try {
                     Intent intent = Intent.parseUri(customAppIntentUri, 0);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -790,19 +813,11 @@ class LockScreen extends LinearLayout implements KeyguardScreen {
     }
 
     private Drawable resize(Drawable image) {
-        Bitmap d = ((BitmapDrawable) image).getBitmap();
-        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, 55, 55, false);
-        return new BitmapDrawable(getContext().getResources(), bitmapOrig);
-    }
+        int size = 100;
 
-    private void launchCustomApp(String uri) {
-        try {
-            Intent intent = Intent.parseUri(uri, 0);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(intent);
-        } catch (URISyntaxException e) {
-            Log.e(TAG, "URISyntaxException: [" + uri + "]");
-        }
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, size, size, false);
+        return new BitmapDrawable(getContext().getResources(), bitmapOrig);
     }
 
     private void updateSettings() {

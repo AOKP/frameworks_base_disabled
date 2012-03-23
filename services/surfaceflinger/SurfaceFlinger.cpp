5207dc2acefca8ea5865c1f9b563cfe0cc7d1d6f
TwistedUmbrella <twistedumbrella@gmail.com>
2012-03-22 23:52:45 -0400

    Add SURFACEFLINGER_FORCE_SCREEN_RELEASE to fix screen off flicker
    That flag name was HUGE, so it was shortened for ease of use. Maybe
    this will solve that obnoxious stutter with changing certain views on
    Shooter. ;)
    
    Conflicts:
    
    	services/surfaceflinger/Android.mk

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1616,7 +1616,7 @@ uint32_t SurfaceFlinger::setClientStateLocked(
 
 void SurfaceFlinger::screenReleased(int dpy)
 {
-#ifdef SURFACEFLINGER_FORCE_SCREEN_RELEASE
+#ifdef SCREEN_RELEASE
     const DisplayHardware& hw = graphicPlane(0).displayHardware();
     hw.releaseScreen();
 #endif
@@ -2141,7 +2141,7 @@ status_t SurfaceFlinger::electronBeamOffAnimationImplLocked()
     glDisable(GL_TEXTURE_2D);
     glDisable(GL_BLEND);
 
-#ifdef SURFACEFLINGER_FORCE_SCREEN_RELEASE
+#ifdef SCREEN_RELEASE
     hw.releaseScreen();
 #endif
 

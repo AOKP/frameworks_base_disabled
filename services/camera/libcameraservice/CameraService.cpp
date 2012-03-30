714ced715a66a5bd493378c55b353f6efce108a7
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-03-30 09:21:29 -0700

    Revert "Revert "Add customized parameters for qcom camera features.""
    This reverts commit ee98f1d424d0e9d776084f322d6863b133284d3a.
    
    Conflicts:
    
    	libs/camera/CameraParameters.cpp

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -365,6 +365,7 @@ CameraService::Client::Client(const sp<CameraService>& cameraService,
     mCameraFacing = cameraFacing;
     mClientPid = clientPid;
     mMsgEnabled = 0;
+    mburstCnt = 0;
     mSurface = 0;
     mPreviewWindow = 0;
     mHardware->setCallbacks(notifyCallback,
@@ -622,6 +623,7 @@ void CameraService::Client::setPreviewCallbackFlag(int callback_flag) {
 // start preview mode
 status_t CameraService::Client::startPreview() {
     LOG1("startPreview (pid %d)", getCallingPid());
+    enableMsgType(CAMERA_MSG_PREVIEW_METADATA);
     return startCameraMode(CAMERA_PREVIEW_MODE);
 }
 
@@ -707,6 +709,7 @@ status_t CameraService::Client::startRecordingMode() {
 // stop preview mode
 void CameraService::Client::stopPreview() {
     LOG1("stopPreview (pid %d)", getCallingPid());
+    disableMsgType(CAMERA_MSG_PREVIEW_METADATA);
     Mutex::Autolock lock(mLock);
     if (checkPidAndHardware() != NO_ERROR) return;
 
@@ -785,6 +788,7 @@ status_t CameraService::Client::cancelAutoFocus() {
 
 // take a picture - image is returned in callback
 status_t CameraService::Client::takePicture(int msgType) {
+    char prop[PROPERTY_VALUE_MAX];
     LOG1("takePicture (pid %d): 0x%x", getCallingPid(), msgType);
 
     Mutex::Autolock lock(mLock);
@@ -806,9 +810,15 @@ status_t CameraService::Client::takePicture(int msgType) {
                            CAMERA_MSG_RAW_IMAGE |
                            CAMERA_MSG_RAW_IMAGE_NOTIFY |
                            CAMERA_MSG_COMPRESSED_IMAGE);
-
+    disableMsgType(CAMERA_MSG_PREVIEW_METADATA);
     enableMsgType(picMsgType);
-
+    memset(prop, 0, sizeof(prop));
+    property_get("persist.camera.snapshot.number", prop, "0");
+    mburstCnt = atoi(prop);
+    if (!mburstCnt) {
+        mburstCnt = mHardware->getParameters().getInt("num-snaps-per-shutter");
+    }
+    LOG1("mburstCnt = %d", mburstCnt);
     return mHardware->takePicture();
 }
 
@@ -898,6 +908,13 @@ status_t CameraService::Client::sendCommand(int32_t cmd, int32_t arg1, int32_t a
     } else if (cmd == CAMERA_CMD_PLAY_RECORDING_SOUND) {
         mCameraService->playSound(SOUND_RECORDING);
     }
+    else if (cmd == CAMERA_CMD_HISTOGRAM_ON ) {
+        enableMsgType(CAMERA_MSG_STATS_DATA);
+    }
+    else if (cmd ==  CAMERA_CMD_HISTOGRAM_OFF) {
+        disableMsgType(CAMERA_MSG_STATS_DATA);
+    }
+
 
     return mHardware->sendCommand(cmd, arg1, arg2);
 }
@@ -1144,8 +1161,12 @@ void CameraService::Client::handleRawPicture(const sp<IMemory>& mem) {
 
 // picture callback - compressed picture ready
 void CameraService::Client::handleCompressedPicture(const sp<IMemory>& mem) {
-    disableMsgType(CAMERA_MSG_COMPRESSED_IMAGE);
+    if (mburstCnt) mburstCnt--;
 
+    if (!mburstCnt) {
+        LOG1("mburstCnt = %d", mburstCnt);
+        disableMsgType(CAMERA_MSG_COMPRESSED_IMAGE);
+    }
     sp<ICameraClient> c = mCameraClient;
     mLock.unlock();
     if (c != 0) {

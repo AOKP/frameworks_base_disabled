1cfe6e2c2fd0d2e52438ea6843f5abd5fa01a657
Illia Ragozin <x0160199@ti.com>
2012-03-30 18:25:22 +0300

    StageFright: enable mp3 audio for mp4 container.
    Enabling mp3 audio in mp4 container, which was not supported in HC.
    
    Change-Id: Ia8ee105a9bcf6d898be259aeb51b8ec2aa7f5b79
    Signed-off-by: Illia Ragozin <x0160199@ti.com>
    
    Conflicts:
    
    	media/libstagefright/MPEG4Extractor.cpp

--- media/libstagefright/MPEG4Extractor.cpp
+++ media/libstagefright/MPEG4Extractor.cpp
@@ -1801,8 +1801,13 @@ status_t MPEG4Extractor::updateAudioTrackInfoFromESDS_MPEG4Audio(
         // The media subtype is MP3 audio
         // Our software MP3 audio decoder may not be able to handle
         // packetized MP3 audio; for now, lets just return ERROR_UNSUPPORTED
-        ALOGE("MP3 track in MP4/3GPP file is not supported");
+#if defined(OMAP_ENHANCEMENT)
+        mLastTrack->meta->setCString(kKeyMIMEType, MEDIA_MIMETYPE_AUDIO_MPEG);
+        return OK;
+#else
+        LOGE("MP3 track in MP4/3GPP file is not supported");
         return ERROR_UNSUPPORTED;
+#endif
     }
 
     const uint8_t *csd;

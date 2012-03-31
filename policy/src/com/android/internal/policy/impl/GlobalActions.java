f07c45b069cc7fce708cd1edffa3724de8a6d150
sethyx <sethyx@gmail.com>
2012-03-31 16:41:56 +0200

    dont show navbar buttons in powermenu if lockscreen is active

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -858,7 +858,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
 
         public boolean showDuringKeyguard() {
-            return true;
+            return false;
         }
 
         public boolean showBeforeProvisioning() {

bcbe54f5f0c661c0ebac9d7e0ba5207e513e09be
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-04-04 00:04:40 -0700

    update media/stagefright/Android.mk

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -1,27 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-    LOCAL_CFLAGS += -DUSE_AAC_HW_DEC
-endif
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
-    LOCAL_CFLAGS += -DTARGET7x27
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-    LOCAL_CFLAGS += -DTARGET7x27A
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-    LOCAL_CFLAGS += -DTARGET7x30
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-    LOCAL_CFLAGS += -DTARGET8x50
-endif
-ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-    LOCAL_CFLAGS += -DTARGET8x60
-endif
-endif
 include frameworks/base/media/libstagefright/codecs/common/Config.mk
 
 LOCAL_SRC_FILES:=                         \
@@ -74,9 +53,10 @@ LOCAL_SRC_FILES:=                         \
         avc_utils.cpp                     \
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-        LOCAL_SRC_FILES += ExtendedExtractor.cpp
-        LOCAL_SRC_FILES += ExtendedWriter.cpp
-	LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
+    LOCAL_SRC_FILES += ExtendedExtractor.cpp
+    LOCAL_SRC_FILES += ExtendedWriter.cpp
+    LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
+
 endif
 
 LOCAL_C_INCLUDES+= \
@@ -85,6 +65,32 @@ LOCAL_C_INCLUDES+= \
         $(TOP)/external/flac/include \
         $(TOP)/external/tremolo \
         $(TOP)/external/openssl/include
+
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+
+LOCAL_C_INCLUDES += \
+    $(TOP)/hardware/qcom/display/libgralloc \
+    $(TOP)/vendor/qcom/opensource/omx/mm-core/omxcore/inc \
+    $(TOP)/system/core/include \
+    $(TOP)/hardware/libhardware_legacy/include \
+    $(TOP)/hardware/qcom/display/libqcomui
+
+LOCAL_CFLAGS += -DQCOM_HARDWARE
+
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+LOCAL_CFLAGS += -DTARGET7x30
+else ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+LOCAL_CFLAGS += -DTARGET8x60
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
+LOCAL_CFLAGS += -DTARGET7x27
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+LOCAL_CFLAGS += -DTARGET7x27A -DUSE_AAC_HW_DEC
+else ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+LOCAL_CFLAGS += -DTARGET8x50
+endif
+
+endif # QCOM_HARDWARE
 
 LOCAL_SHARED_LIBRARIES := \
         libbinder         \
@@ -182,13 +188,6 @@ LOCAL_SHARED_LIBRARIES += \
         libdl
 
 LOCAL_CFLAGS += -Wno-multichar
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libgralloc
-        LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/omx/mm-core/omxcore/inc
-        LOCAL_C_INCLUDES += $(TOP)/system/core/include
-        LOCAL_C_INCLUDES += $(TOP)/hardware/libhardware_legacy/include
-endif
 
 LOCAL_MODULE:= libstagefright
 

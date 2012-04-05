585c7584b37b28569f8fa0914b8aa78cdef3a399
TwistedUmbrella <twistedumbrella@gmail.com>
2012-04-05 16:27:54 -0400

    Restore "Select texture target based on pixel format header"

--- include/gui/SurfaceTexture.h
+++ include/gui/SurfaceTexture.h
@@ -141,7 +141,12 @@ public:
     //
     // This call may only be made while the OpenGL ES context to which the
     // target texture belongs is bound to the calling thread.
+#ifndef QCOM_HARDWARE
     status_t updateTexImage();
+#else
+    //
+    status_t updateTexImage(bool isComposition  = false);
+#endif
 
     // setBufferCountServer set the buffer count. If the client has requested
     // a buffer count using setBufferCount, the server-buffer count will

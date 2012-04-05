09623751272104f7624251922520399464dee8da
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-04-04 20:52:22 -0700

    Revert "Do not check for framerate."
    This reverts commit a4cd28de7d0cc3cef792dbde440b10fca7cf2696.

--- media/libstagefright/CameraSource.cpp
+++ media/libstagefright/CameraSource.cpp
@@ -436,9 +436,9 @@ status_t CameraSource::checkFrameRate(
     // Check the actual video frame rate against the target/requested
     // video frame rate.
     if (frameRate != -1 && (frameRateActual - frameRate) != 0) {
-        LOGE("APP set preview frame rate to %d fps. The actual "
+        LOGE("Failed to set preview frame rate to %d fps. The actual "
                 "frame rate is %d", frameRate, frameRateActual);
-        /* return UNKNOWN_ERROR; */
+        return UNKNOWN_ERROR;
     }
 
     // Good now.

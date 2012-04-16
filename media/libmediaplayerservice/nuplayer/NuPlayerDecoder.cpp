ffdeecb152456f394268a13aaae18e22d1152f0a
Liquid Smooth <liquid0624@gmail.com>
2012-04-16 01:33:21 -0400

    Revert "Set Software AAC Decoder as default for RTSP based Streaming"
    This reverts commit a6ff332d2513dcaaac132e9d6996e8e4a22fbf8d.

--- media/libmediaplayerservice/nuplayer/NuPlayerDecoder.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerDecoder.cpp
@@ -127,12 +127,6 @@ sp<AMessage> NuPlayer::Decoder::makeFormat(const sp<MetaData> &meta) {
 
         msg->setInt32("channel-count", numChannels);
         msg->setInt32("sample-rate", sampleRate);
-
-        int32_t useSWDecforAudio;
-        if(meta->findInt32(kKeyUseSWDec, &useSWDecforAudio)) {
-            LOGV("Use Software Decoder for Audio");
-            msg->setInt32("use-swdec", useSWDecforAudio);
-        }
     }
 
     int32_t maxInputSize;

2ee15de8bdda0a1ca06368e6eb86fcd14c7492f1
Mike Wilson <mwilson@acesappraisal.com>
2012-04-19 18:20:36 -0400

    Really add NavBar Hide feature this time. It got squashed somehow in last set of commits.
    Change-Id: Ifd5280f99a209078fbe967c031712ec7bddcce89
    Signed-off-by: Mike Wilson <mwilson@acesappraisal.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -2055,6 +2055,9 @@ public class TabletStatusBar extends StatusBar implements
                         Settings.System.getUriFor(Settings.System.NAVIGATION_CUSTOM_APP_ICONS[j]),
                         false,
                         this);
+                resolver.registerContentObserver(
+                		Settings.System.getUriFor(Settings.System.NAVIGATION_BAR_BUTTONS_SHOW), false,
+                		this);
             }
             updateSettings();
         }
@@ -2099,6 +2102,9 @@ public class TabletStatusBar extends StatusBar implements
                 Settings.System.putString(resolver,
                 		Settings.System.NAVIGATION_CUSTOM_APP_ICONS[j], "");
             }
+            mShowStatusBar = (Settings.System.getInt(resolver,
+                    Settings.System.NAVIGATION_BAR_BUTTONS_SHOW, 1) == 1);
+            mStatusBarView.setVisibility(mShowStatusBar ? View.VISIBLE : View.GONE);
         }
         makeNavBar();
 

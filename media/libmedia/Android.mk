532b20f8db634fc16366e1ae6a3ea683bcc55665
Daniel Bateman <jetison.24@gmail.com>
2012-04-24 00:10:17 -0500

    libmedia: don't require USES_AUDIO_LEGACY for USE_KINETO_COMPATIBILITY
    Change-Id: I74ff9c0c4dd0cafa20917e1e923f1d0d2d6b1df4

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -52,15 +52,16 @@ ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
         AudioParameter.cpp
 
     LOCAL_CFLAGS += -DUSES_AUDIO_LEGACY
-    ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
-        LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
-    endif
+endif
+
+ifeq ($(BOARD_USE_KINETO_COMPATIBILITY),true)
+    LOCAL_CFLAGS += -DUSE_KINETO_COMPATIBILITY
 endif
 
 LOCAL_SHARED_LIBRARIES := \
-	libui libcutils libutils libbinder libsonivox libicuuc libexpat \
-        libcamera_client libstagefright_foundation \
-        libgui libdl
+    libui libcutils libutils libbinder libsonivox libicuuc libexpat \
+    libcamera_client libstagefright_foundation \
+    libgui libdl
 
 
 LOCAL_WHOLE_STATIC_LIBRARY := libmedia_helper

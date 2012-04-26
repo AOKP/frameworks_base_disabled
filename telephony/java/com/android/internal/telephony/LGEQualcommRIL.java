cbf7bb4a1f88cb0dbbf290b8b9eef4794e63ac47
Ricardo Cerqueira <github@cerqueira.org>
2012-04-26 22:30:19 +0100

    LGEQualcommRIL: Fix dataCallState objects
    Change-Id: I5f36370981737200de808922bd6772a08dc85608

--- telephony/java/com/android/internal/telephony/LGEQualcommRIL.java
+++ telephony/java/com/android/internal/telephony/LGEQualcommRIL.java
@@ -180,7 +180,7 @@ public class LGEQualcommRIL extends QualcommNoSimReadyRIL implements CommandsInt
         rr.mp.writeString(user);
         rr.mp.writeString(password);
         rr.mp.writeString(authType);
-        rr.mp.writeString("0"); // ipVersion
+        rr.mp.writeString("IP"); // ipVersion
 
         if (RILJ_LOGD) riljLog(rr.serialString() + "> "
                 + requestToString(rr.mRequest) + " " + radioTechnology + " "
@@ -362,6 +362,11 @@ public class LGEQualcommRIL extends QualcommNoSimReadyRIL implements CommandsInt
         dataCall.ifname = "rmnet0";
         p.readInt(); // RadioTechnology
         p.readInt(); // inactiveReason
+
+        dataCall.dnses = new String[2];
+        dataCall.dnses[0] = SystemProperties.get("net."+dataCall.ifname+".dns1");
+        dataCall.dnses[1] = SystemProperties.get("net."+dataCall.ifname+".dns2");
+
         return dataCall;
     }
 
@@ -383,6 +388,13 @@ public class LGEQualcommRIL extends QualcommNoSimReadyRIL implements CommandsInt
         if (!TextUtils.isEmpty(addresses)) {
           dataCall.addresses = addresses.split(" ");
         }
+
+        dataCall.dnses = new String[2];
+        dataCall.dnses[0] = SystemProperties.get("net."+dataCall.ifname+".dns1");
+        dataCall.dnses[1] = SystemProperties.get("net."+dataCall.ifname+".dns2");
+        dataCall.active = 1;
+        dataCall.status = 0;
+
         return dataCall;
     }
 

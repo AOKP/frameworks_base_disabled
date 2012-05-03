2a79f5ec542de932bcb2934c02e0a4f6234630e4
Anviksha Khare <anviksha@ti.com>
2012-05-03 16:36:37 +0530

    AVI extractor: Save fps information
    Calculating fps (dwRate/dwScale) in .avi container and
    saving it as kKeyVideoFPS for future reference.
    This information was missing, hence VC1 60fps clips were
    playing at 30fps, hence resulting in issues for 60fps.
    Fix the same.
    
    Change-Id: I5d1de853807f92c33ef976e97a2c83ad80184213
    Signed-off-by: Anviksha Khare <anviksha@ti.com>

--- media/libstagefright/AVIExtractor.cpp
+++ media/libstagefright/AVIExtractor.cpp
@@ -611,7 +611,10 @@ status_t AVIExtractor::parseStreamHeader(off64_t offset, size_t size) {
 
     uint32_t rate = U32LE_AT(&data[20]);
     uint32_t scale = U32LE_AT(&data[24]);
-
+#ifdef OMAP_ENHANCEMENT
+    //Calculating fps and rounding to the closest integer value
+    uint32_t fps=(scale * 1.0 / rate) + 0.5;
+#endif
     uint32_t sampleSize = U32LE_AT(&data[44]);
 
     const char *mime = NULL;
@@ -661,7 +664,9 @@ status_t AVIExtractor::parseStreamHeader(off64_t offset, size_t size) {
     track->mMaxSampleSize = 0;
     track->mAvgChunkSize = 1.0;
     track->mFirstChunkSize = 0;
-
+#ifdef OMAP_ENHANCEMENT
+    track->mMeta->setInt32(kKeyVideoFPS,fps);
+#endif
     return OK;
 }
 

03f2837c3266f39c46f2a45565d2a099eb9a844e
nocoast <voteforpedro8@gmail.com>
2012-05-08 13:12:58 -0500

    Nebkat Sayed this was the derp. THANK YOU NEBKAT REBOOTING INTO UBUNTU AND TEST BUILDING NOW ;)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1910,8 +1910,7 @@ public final class Settings {
          * pending. The value is boolean (1 or 0).
          * @hide
          */
-        public static final String NOTIFICATION_LIGHT_PULSE = "notification_light_pulse_custom_values";
-
+public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES = "notification_light_pulse_custom_values";
         /**
          * Whether the battery LED should repeatedly flash when the battery is low
          * on charge. The value is boolean (1 or 0).

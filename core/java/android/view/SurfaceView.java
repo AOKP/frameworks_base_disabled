41d616052b2d19f77c6f72cec755b7098db1fb38
Yuriy Zabroda <yuriy.zabroda@ti.com>
2012-05-17 17:15:34 +0300

    Add extra check to updateWindow method of SurfaceView
    The change forbids calling of Surface callbacks when Surface become invalid.
    This prevents the system from displaying misleading alerts on the cases like shown below.
    
    1. Playing a movie (especially Full HD) by Gallery.
    2. Press the Home button.
    3. Press the Gallery shortcut on Launcher.
    4. Very quickly repeat #2 and #3 several times.
    
    Result: "Sorry, this video cannot be played" alert is shown.
    
    Change-Id: Iaf43e40a1405af2b293118f48e8153844f9e20bc
    Signed-off-by: Yuriy Zabroda <yuriy.zabroda@ti.com>
    
    Conflicts:
    
    	core/java/android/view/SurfaceView.java

--- core/java/android/view/SurfaceView.java
+++ core/java/android/view/SurfaceView.java
@@ -31,6 +31,7 @@ import android.os.Message;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.ParcelFileDescriptor;
+import android.os.SystemProperties;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.os.SystemProperties;
@@ -535,8 +536,14 @@ public class SurfaceView extends View {
                             if (callbacks == null) {
                                 callbacks = getSurfaceCallbacks();
                             }
-                            for (SurfaceHolder.Callback c : callbacks) {
-                                if (mSurface.isValid()) {
+                            if (SystemProperties.OMAP_ENHANCEMENT) {
+                                for (SurfaceHolder.Callback c : callbacks) {
+                                    if (mSurface.isValid()) {
+                                        c.surfaceCreated(mSurfaceHolder);
+                                    }
+                                }
+                            } else {
+                                for (SurfaceHolder.Callback c : callbacks) {
                                     c.surfaceCreated(mSurfaceHolder);
                                 }
                             }
@@ -548,8 +555,14 @@ public class SurfaceView extends View {
                             if (callbacks == null) {
                                 callbacks = getSurfaceCallbacks();
                             }
-                            for (SurfaceHolder.Callback c : callbacks) {
-                                if (mSurface.isValid()) {
+                            if (SystemProperties.OMAP_ENHANCEMENT) {
+                                for (SurfaceHolder.Callback c : callbacks) {
+                                    if (mSurface.isValid()) {
+                                        c.surfaceChanged(mSurfaceHolder, mFormat, myWidth, myHeight);
+                                    }
+                                }
+                            } else {
+                                for (SurfaceHolder.Callback c : callbacks) {
                                     c.surfaceChanged(mSurfaceHolder, mFormat, myWidth, myHeight);
                                 }
                             }
@@ -559,9 +572,18 @@ public class SurfaceView extends View {
                             if (callbacks == null) {
                                 callbacks = getSurfaceCallbacks();
                             }
-                            for (SurfaceHolder.Callback c : callbacks) {
-                                if (c instanceof SurfaceHolder.Callback2) {
-                                    if (mSurface.isValid()) {
+                            if (SystemProperties.OMAP_ENHANCEMENT) {
+                                for (SurfaceHolder.Callback c : callbacks) {
+                                    if (c instanceof SurfaceHolder.Callback2) {
+                                        if (mSurface.isValid()) {
+                                            ((SurfaceHolder.Callback2)c).surfaceRedrawNeeded(
+                                                    mSurfaceHolder);
+                                        }
+                                    }
+                                }
+                            } else {
+                                for (SurfaceHolder.Callback c : callbacks) {
+                                    if (c instanceof SurfaceHolder.Callback2) {
                                         ((SurfaceHolder.Callback2)c).surfaceRedrawNeeded(
                                                 mSurfaceHolder);
                                     }

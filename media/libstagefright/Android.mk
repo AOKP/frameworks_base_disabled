d3ca493316c7708e77687078d4aaff395a0a06f4
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-05-18 08:16:09 -0700

    stagefright: add uniformity to Android.mk
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>
    Change-Id: Ia163f2234e34e56fd10847dfff20a3f337b4aa88

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -1,6 +1,33 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+    LOCAL_CFLAGS += -DUSE_AAC_HW_DEC
+endif
+
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
+    LOCAL_CFLAGS += -DTARGET7x27
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
+    LOCAL_CFLAGS += -DTARGET7x27A
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    LOCAL_CFLAGS += -DTARGET7x30
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    LOCAL_CFLAGS += -DTARGET8x50
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    LOCAL_CFLAGS += -DTARGET8x60
+endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+    LOCAL_CFLAGS += -DTARGET8x60
+endif
+ifeq ($(BOARD_CAMERA_USE_MM_HEAP),true)
+    LOCAL_CFLAGS += -DCAMERA_MM_HEAP
+endif
+endif
 include frameworks/base/media/libstagefright/codecs/common/Config.mk
 
 LOCAL_SRC_FILES:=                         \
@@ -53,10 +80,9 @@ LOCAL_SRC_FILES:=                         \
         avc_utils.cpp                     \
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    LOCAL_SRC_FILES += ExtendedExtractor.cpp
-    LOCAL_SRC_FILES += ExtendedWriter.cpp
-    LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
-
+        LOCAL_SRC_FILES += ExtendedExtractor.cpp
+        LOCAL_SRC_FILES += ExtendedWriter.cpp
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
 endif
 
 LOCAL_C_INCLUDES+= \
@@ -65,37 +91,6 @@ LOCAL_C_INCLUDES+= \
         $(TOP)/external/flac/include \
         $(TOP)/external/tremolo \
         $(TOP)/external/openssl/include
-
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-
-LOCAL_C_INCLUDES += \
-    $(TOP)/hardware/qcom/display/libgralloc \
-    $(TOP)/vendor/qcom/opensource/omx/mm-core/omxcore/inc \
-    $(TOP)/system/core/include \
-    $(TOP)/hardware/libhardware_legacy/include \
-    $(TOP)/hardware/qcom/display/libqcomui
-
-LOCAL_CFLAGS += -DQCOM_HARDWARE
-
-ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-LOCAL_CFLAGS += -DTARGET7x30
-else ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
-LOCAL_CFLAGS += -DTARGET8x60
-else ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-LOCAL_CFLAGS += -DTARGET8x60
-else ifeq ($(TARGET_BOARD_PLATFORM),msm7x27)
-LOCAL_CFLAGS += -DTARGET7x27
-else ifeq ($(TARGET_BOARD_PLATFORM),msm7x27a)
-LOCAL_CFLAGS += -DTARGET7x27A -DUSE_AAC_HW_DEC
-else ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-LOCAL_CFLAGS += -DTARGET8x50
-else ifeq ($(BOARD_CAMERA_USE_MM_HEAP),true)
-LOCAL_CFLAGS += -DCAMERA_MM_HEAP
-endif
-
-
-endif # QCOM_HARDWARE
 
 LOCAL_SHARED_LIBRARIES := \
         libbinder         \
@@ -193,6 +188,14 @@ LOCAL_SHARED_LIBRARIES += \
         libdl
 
 LOCAL_CFLAGS += -Wno-multichar
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libgralloc
+        LOCAL_C_INCLUDES += $(TOP)/vendor/qcom/opensource/omx/mm-core/omxcore/inc
+        LOCAL_C_INCLUDES += $(TOP)/system/core/include
+        LOCAL_C_INCLUDES += $(TOP)/hardware/libhardware_legacy/include
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display/libqcomui
+endif
 
 LOCAL_MODULE:= libstagefright
 

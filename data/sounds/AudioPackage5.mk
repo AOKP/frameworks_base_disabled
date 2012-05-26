06b06d8ec11c89390481d7f76db79212b7b8f0c5
Matanya Elchanani <melchanani@hotmail.com>
2012-05-26 11:39:08 -0400

    Remove duplicate ringtone
    Change-Id: I360601fe861e1a9084081dde66e7466d408435c2

--- data/sounds/AudioPackage5.mk
+++ data/sounds/AudioPackage5.mk
@@ -47,7 +47,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/ringtones/Aquila.ogg:system/media/audio/ringtones/Aquila.ogg \
 	$(LOCAL_PATH)/ringtones/ArgoNavis.ogg:system/media/audio/ringtones/ArgoNavis.ogg \
 	$(LOCAL_PATH)/ringtones/BOOTES.ogg:system/media/audio/ringtones/BOOTES.ogg \
-	$(LOCAL_PATH)/ringtones/CANISMAJOR.ogg:system/media/audio/ringtones/CANISMAJOR.ogg \
 	$(LOCAL_PATH)/ringtones/Carina.ogg:system/media/audio/ringtones/Carina.ogg \
 	$(LOCAL_PATH)/ringtones/CASSIOPEIA.ogg:system/media/audio/ringtones/CASSIOPEIA.ogg \
 	$(LOCAL_PATH)/ringtones/Centaurus.ogg:system/media/audio/ringtones/Centaurus.ogg \

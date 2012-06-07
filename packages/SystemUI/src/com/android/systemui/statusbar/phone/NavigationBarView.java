c2c0aa1422c94c1f2e3fb5e6703e190cab9cbaad
Blueharford <blueharford@gmail.com>
2012-06-07 14:37:33 -0700

    change icon for nav widgets
    Change-Id: Ie9ea9c02cab73daebcce29ce83c71c9db1aa13d8

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -806,7 +806,7 @@ public class NavigationBarView extends LinearLayout {
     private Drawable getNavbarIconImage(boolean landscape, String uri) {
 
         if (uri == null)
-            return getResources().getDrawable(R.drawable.ic_sysbar_null);
+            return getResources().getDrawable(R.drawable.ic_notify_liquid);
 
         if (uri.startsWith("**")) {
             if (uri.equals(ACTION_HOME)) {

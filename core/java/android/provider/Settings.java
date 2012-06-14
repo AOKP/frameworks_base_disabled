a74b2cac9806c8072e10d4d1b5549330d17dd120
sethyx <sethyx@gmail.com>
2012-06-14 20:06:01 +0200

    NFC polling mode (3/3)
    Change-Id: Ifb3fc5036c3ae0b71829d7249f4f1b951c5e7a5d
    Signed-off-by: sethyx <sethyx@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2355,6 +2355,13 @@ public final class Settings {
         public static final String TABLET_UI = "tablet_ui";
 
         /**
+         * NFC polling mode configuration key
+         *
+         * @hide
+         */
+        public static final String NFC_POLLING_MODE = "nfc_polling_mode";
+
+        /**
          * @hide
          */
         public static final String STATUSBAR_TOGGLES_USE_BUTTONS = "statusbar_toggles_use_buttons";

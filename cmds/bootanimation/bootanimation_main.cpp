dded5147ea87c71ee14449ee4c3b90dcb6b6c6ba
Blueharford <jsseidel1@gmail.com>
2012-06-18 13:43:36 -0700

    enabled stock boot animation disabled
    Change-Id: I2505e4af43f938f116c8d56e4eb2a7485df7c2b0

--- cmds/bootanimation/bootanimation_main.cpp
+++ cmds/bootanimation/bootanimation_main.cpp
@@ -45,7 +45,7 @@ int main(int argc, char** argv)
 #endif
 
     char value[PROPERTY_VALUE_MAX];
-    property_get("persist.sf.nobootanimation", value, "0");
+    property_get("persist.sys.nobootanimation", value, "0");
     int noBootAnimation = atoi(value);
     ALOGI_IF(noBootAnimation,  "boot animation disabled");
     if (!noBootAnimation) {

7f44c9456f89c72355b881afccb30e0088ab4e04
Liquid Smooth <liquid0624@gmail.com>
2012-06-21 21:56:32 -0400

    Revert "Power Widget: Add toggle profile button (1/2)"
    This reverts commit 18ac8554c7404c1674a823bfc28956be1ac14665.

--- packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/powerwidget/PowerButton.java
@@ -1,9 +1,6 @@
 package com.android.systemui.statusbar.powerwidget;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.android.systemui.R;
 
 import android.app.ActivityManagerNative;
 import android.content.ContentResolver;
@@ -12,8 +9,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
+import android.graphics.PorterDuff.Mode;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
@@ -23,16 +20,10 @@ import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.ImageView;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.android.systemui.R;
-
-/**
- * THIS CLASS'S DATA MUST BE KEPT UP-TO-DATE WITH THE DATA IN
- * com.android.systemui.statusbar.powerwidget.PowerWidget AND
- * com.android.settings.cyanogenmod.PowerWidgetUtil
- */
 public abstract class PowerButton {
     public static final String TAG = "PowerButton";
     public static final boolean DEBUG = false;
@@ -65,7 +56,7 @@ public abstract class PowerButton {
     public static final String BUTTON_WIMAX = "toggleWimax";
     public static final String BUTTON_LTE = "toggleLte";
     public static final String BUTTON_UNKNOWN = "unknown";
-    public static final String BUTTON_PROFILE = "toggleProfile";
+
     private static final Mode MASK_MODE = Mode.SCREEN;
 
     protected int mIcon;
@@ -190,6 +181,7 @@ public abstract class PowerButton {
     private View.OnLongClickListener mLongClickListener = new View.OnLongClickListener() {
         public boolean onLongClick(View v) {
             boolean result = handleLongClick(v.getContext());
+
             if (result && mHapticFeedback && mLongClickPattern != null) {
                 mVibrator.vibrate(mLongClickPattern, -1);
             }

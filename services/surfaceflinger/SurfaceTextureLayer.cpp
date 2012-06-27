205d212cbb17e6c54bcd464931d7b3d29f644329
Jonathan Bither <jonbither@gmail.com>
2012-06-27 17:41:10 -0400

    surfaceflinger: fix pre-rotation
    Fix courtesy Jonas.Larsson@ti.com
    
    The method for SurfaceTextureLayer queueBuffer() had the wrong
    set of arguments so the parent class (SurfaceTexture) method
    was being called without saving the layer transform hint.
    
    Change-Id: Ifcfd7ee616d9d77b0c61997a7133119053a66d8b
    Signed-off-by: Tony Lofthouse <a0741364@ti.com>
    
    Conflicts:
    
    	services/surfaceflinger/SurfaceTextureLayer.cpp

--- services/surfaceflinger/SurfaceTextureLayer.cpp
+++ services/surfaceflinger/SurfaceTextureLayer.cpp
@@ -82,8 +82,20 @@ status_t SurfaceTextureLayer::queueBuffer(int buf, int64_t timestamp,
             outWidth, outHeight, outTransform, metadata);
 #else
 
+#ifdef OMAP_ENHANCEMENT
+status_t SurfaceTextureLayer::queueBuffer(int buf, int64_t timestamp,
+        uint32_t* outWidth, uint32_t* outHeight, uint32_t* outTransform,
+        const String8& metadata) {
+
+   status_t res = SurfaceTexture::queueBuffer(buf, timestamp,
+            outWidth, outHeight, outTransform, metadata);
+#else
+
 status_t SurfaceTextureLayer::queueBuffer(int buf, int64_t timestamp,
         uint32_t* outWidth, uint32_t* outHeight, uint32_t* outTransform) {
+
+    status_t res = SurfaceTexture::queueBuffer(buf, timestamp,
+            outWidth, outHeight, outTransform);
 #endif
     sp<Layer> layer(mLayer.promote());
     if (layer != NULL) {

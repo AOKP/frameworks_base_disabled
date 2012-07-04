20efabda6f1b7d590f7e2af0fcf6d9c544b35f8b
Jonathan Bither <jonbither@gmail.com>
2012-07-03 21:50:26 -0400

    Add "OMAP buffers are interlaced, try to accomidate this functionality" to reduce the number of google chrome errors

--- libs/gui/SurfaceTextureClient.cpp
+++ libs/gui/SurfaceTextureClient.cpp
@@ -192,6 +192,9 @@ int SurfaceTextureClient::getSlotFromBufferLocked(
     for (int i = 0; i < NUM_BUFFER_SLOTS; i++) {
         // XXX: Dump the slots whenever we hit a NULL entry while searching for
         // a buffer.
+#ifdef OMAP_ENHANCEMENT
+        if (mSlots[i] == NULL) i++;
+#endif
         if (mSlots[i] == NULL) {
             if (!dumpedState) {
                 ALOGD("getSlotFromBufferLocked: encountered NULL buffer in slot %d "

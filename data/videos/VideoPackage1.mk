2c18603109c95176cd66f539ec5ecf2de9720870
Roman Birg <romanbirg@gmail.com>
2012-07-16 22:37:11 -0400

    add prebuilts for extra camera effects
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- data/videos/VideoPackage1.mk
+++ data/videos/VideoPackage1.mk
@@ -22,5 +22,11 @@ TARGET_PATH := system/media/video
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/AndroidInSpace.240p.mp4:$(TARGET_PATH)/AndroidInSpace.240p.mp4 \
         $(LOCAL_PATH)/AndroidInSpace.480p.lq.mp4:$(TARGET_PATH)/AndroidInSpace.480p.mp4 \
+        $(LOCAL_PATH)/Disco.240p.mp4:$(TARGET_PATH)/Disco.240p.mp4 \
+        $(LOCAL_PATH)/Disco.480p.lq.mp4:$(TARGET_PATH)/Disco.480p.mp4 \
         $(LOCAL_PATH)/Sunset.240p.mp4:$(TARGET_PATH)/Sunset.240p.mp4 \
         $(LOCAL_PATH)/Sunset.480p.lq.mp4:$(TARGET_PATH)/Sunset.480p.mp4
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/LMprec_508.emd:system/media/LMprec_508.emd \
+        $(LOCAL_PATH)/PFFprec_600.emd:system/media/PFFprec_600.emd

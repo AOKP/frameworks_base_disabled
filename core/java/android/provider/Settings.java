e7fc9618be6c054e4500ac6121d57074d524c716
xoomdev <xoomdevnet@gmail.com>
2012-08-09 08:09:11 -0500

    Bring back nav bar color, glow color, and glow duration from ICS
    Change-Id: I2949a78268e33ae8b27de94f312e60367f61bf89

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2658,6 +2658,27 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_TINT = "navigation_bar_tint";
+
+        /**
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_GLOW_TINT = "navigation_bar_glow_tint";
+
+        /**
+         * [0] = how long to animate glow off
+         * [1] = how long to animate glow on
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_BAR_GLOW_DURATION = new String[] {
+            "navigation_bar_glow_duration_off",
+            "navigation_bar_glow_duration_on"
+        };
+
+        /**
          * How long to keep the notification LED on (in milliseconds)
          *
          * @hide

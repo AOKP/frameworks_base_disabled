8fe27e69f28ab593e3baa2b961552b4f1ef86d52
NewbyJE <NewbyJE@comcast.net>
2012-08-15 18:02:02 -0700

    telephony: neighboring cells are now handled properly
    GsmServiceStateTracker.java sets the System Property
    PROPERTY_DATA_NETWORK_TYPE = "TypeName:RadioTechnology"
    for the current network type (ex. EDGE:2 or HSPA:11).
    
    When neighboring cell info is sent to responseCellList() in RIL.java,
    the routine compares PROPERTY_DATA_NETWORK_TYPE to just the TypeName
    (without the :) and can not find a match.  It defaults to
    NETWORK_TYPE_UNKNOWN and does no interpret the location.
    
    This patch splits off the string part of PROPERTY_DATA_NETWORK_TYPE
    so a proper match is found and the location is interpreted.
    
    Also HSPA+ (HSPAP) is added as a valid match.
    
    Patch compiled and tested on HP TouchPad 4G.
    
    Change-Id: I223db80d6c4a7791300e299826aa316854738d5c

--- telephony/java/com/android/internal/telephony/RIL.java
+++ telephony/java/com/android/internal/telephony/RIL.java
@@ -3365,6 +3365,10 @@ public class RIL extends BaseCommands implements CommandsInterface {
        // Determine the radio access type
        String radioString = SystemProperties.get(
                TelephonyProperties.PROPERTY_DATA_NETWORK_TYPE, "unknown");
+       if (radioString.contains(":")) {
+           String[] parts = radioString.split(":");
+           radioString = parts[0];
+       }
        int radioType;
        if (radioString.equals("GPRS")) {
            radioType = NETWORK_TYPE_GPRS;

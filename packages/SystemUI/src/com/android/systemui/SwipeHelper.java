a7f9307a7e770b171e3655916981a78166890574
LorD ClockaN <davor@losinj.com>
2012-08-17 23:54:35 +0200

    Let's double the speed of notifications/statusbar animations

--- packages/SystemUI/src/com/android/systemui/SwipeHelper.java
+++ packages/SystemUI/src/com/android/systemui/SwipeHelper.java
@@ -46,10 +46,10 @@ public class SwipeHelper implements Gefingerpoken {
 
     private static LinearInterpolator sLinearInterpolator = new LinearInterpolator();
 
-    private float SWIPE_ESCAPE_VELOCITY = 100f; // dp/sec
-    private int DEFAULT_ESCAPE_ANIMATION_DURATION = 200; // ms
-    private int MAX_ESCAPE_ANIMATION_DURATION = 400; // ms
-    private int MAX_DISMISS_VELOCITY = 2000; // dp/sec
+    private float SWIPE_ESCAPE_VELOCITY = 200f; // dp/sec
+    private int DEFAULT_ESCAPE_ANIMATION_DURATION = 100; // ms
+    private int MAX_ESCAPE_ANIMATION_DURATION = 200; // ms
+    private int MAX_DISMISS_VELOCITY = 4000; // dp/sec
     private static final int SNAP_ANIM_LEN = SLOW_ANIMATIONS ? 1000 : 150; // ms
 
     public static float ALPHA_FADE_START = 0f; // fraction of thumbnail width

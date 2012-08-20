822cbbea135f0877d4ba616639b2e5b2bc34a9a9
Konsta <konsta09@gmail.com>
2012-08-20 10:39:57 +0300

    Frameworks: Option to control cursor in text fields using volume keys (1/2)
    This feature is moved to framework so it also works with third party keyboards.
    Patch Set 3: Added missin @hide statements
    
    Change-Id: I8e20240e7bee5351ab20bb3d701eb95a5fd3e112

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3030,6 +3030,15 @@ public final class Settings {
          */
         public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
 
+         /**
+          * Volume keys control cursor in text fields (default is 0)
+          * 0 - Disabled
+          * 1 - Volume up/down moves cursor left/right
+          * 2 - Volume up/down moves cursor right/left
+          * @hide
+          */
+         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
+
         /**
          * @hide
          */

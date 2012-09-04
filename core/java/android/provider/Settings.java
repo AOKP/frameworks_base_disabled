79788f49cb8423b9e8b20bf9287418c7669ae89b
LorDClockaN <davor@losinj.com>
2012-09-04 14:38:51 +0200

    SystemUI: WiFi Signal Text
    Change-Id: I926787c92d8f054370a231ecd668a4bf352d832f
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3103,6 +3103,20 @@ public final class Settings {
          */
         public static final String STATUSBAR_HIDE_SIGNAL_BARS = "statusbar_hide_signal_bars";
 
+        /**
+         * Whether to show statusbar WiFi signal text
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_WIFI_SIGNAL_TEXT = "statusbar_wifi_signal_text";
+
+        /**
+         * statusbar WIFI signal text color
+         *
+         * @hide
+         */
+        public static final String STATUSBAR_WIFI_SIGNAL_TEXT_COLOR = "statusbar_wifi_signal_text_color";
+
 	/**
          * whether to enable end app on back longpress functionality
          *

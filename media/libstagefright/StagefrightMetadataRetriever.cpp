CM9-D2G/android_framework_base

Fix a thumbnail generation issue

media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -390,7 +390,7 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
         memcpy(mAlbumArt->mData, data, dataSize);
     }
 
-#ifdef QCOM_HARDWARE
+#if defined(QCOM_HARDWARE)
     const char *mime;
     bool success = trackMeta->findCString(kKeyMIMEType, &mime);
     CHECK(success);
@@ -405,14 +405,44 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
             mime);
     } else {
         frame = extractVideoFrameWithCodecFlags(
-#else
-    VideoFrame *frame =
-        extractVideoFrameWithCodecFlags(
-#endif
                 &mClient, trackMeta, source, OMXCodec::kPreferSoftwareCodecs,
                 timeUs, option);
-#ifdef QCOM_HARDWARE
     }
+#elif defined(OMAP_ENHANCEMENT)
+    const char *filemime;
+    CHECK(fileMeta->findCString(kKeyMIMEType, &filemime));
+    LOGV("file mime type:%s",filemime);
+    VideoFrame *frame=NULL;
+    bool useHWCodec =0;
+
+    /*For all the formats except Mpeg2TS/Mpeg2PS, we try with Software decoder
+    first. Currently there is no support for seek in Mpeg2TSExtracto or
+    Mpeg2PSExtractor. Incase of Mpeg2TS  if software decoder fails to decode,
+    the extractor is providing the next available frame instead of seeking back
+    to I-frame. So, incase of Mpeg2TS/Mpeg2PS clips we use hardware decoder
+    for thumbnail generation*/
+    if ((!strcasecmp(filemime, MEDIA_MIMETYPE_CONTAINER_MPEG2TS))
+        ||(!strcasecmp(filemime, MEDIA_MIMETYPE_CONTAINER_MPEG2PS))){
+            useHWCodec = 1;
+    }
+    if (!useHWCodec){
+        LOGV("Trying with s/w codec(NonMpeg2TS/PS clip)");
+        frame =  extractVideoFrameWithCodecFlags(
+           &mClient, trackMeta, source, OMXCodec::kPreferSoftwareCodecs,
+           timeUs, option);
+    }
+    if (frame == NULL){
+       LOGV("Software decoder failed to extract thumbnail, "
+           "trying hardware decoder.");
+       frame = extractVideoFrameWithCodecFlags(&mClient, trackMeta, source, 0,
+                     timeUs, option);
+    }
+#else
+
+    VideoFrame *frame =
+        extractVideoFrameWithCodecFlags(
+                &mClient, trackMeta, source, OMXCodec::kPreferSoftwareCodecs,
+                timeUs, option);
 #endif
 
 #if defined(TARGET8x60) || !defined(QCOM_HARDWARE)

AOKP/frameworks_base_disabled

Conflicts:
	media/libstagefright/StagefrightMetadataRetriever.cpp

media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -384,22 +384,38 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
         memcpy(mAlbumArt->mData, data, dataSize);
     }
 
-#ifdef QCOM_HARDWARE
-    const char *mime;
-    bool success = trackMeta->findCString(kKeyMIMEType, &mime);
-    CHECK(success);
-    VideoFrame *frame = NULL;
 
-    if ((!strcmp(mime, MEDIA_MIMETYPE_VIDEO_DIVX))||
-            (!strcmp(mime, MEDIA_MIMETYPE_VIDEO_DIVX311))||
-            (!strcmp(mime, MEDIA_MIMETYPE_VIDEO_DIVX4))||
-            (!strcmp(mime, MEDIA_MIMETYPE_VIDEO_WMV)))
-    {
-        LOGV("Software codec is not being used for %s clips for thumbnail ",
-            mime);
-    } else {
-        frame = extractVideoFrameWithCodecFlags(
+#if defined(OMAP_ENHANCEMENT)
+    const char *filemime;
+    CHECK(fileMeta->findCString(kKeyMIMEType, &filemime));
+    LOGV("file mime type:%s",filemime);
+    VideoFrame *frame=NULL;
+    bool useHWCodec =0;
+
+    /*For all the formats except Mpeg2TS/Mpeg2PS, we try with Software decoder
+    first. Currently there is no support for seek in Mpeg2TSExtracto or
+    Mpeg2PSExtractor. Incase of Mpeg2TS  if software decoder fails to decode,
+    the extractor is providing the next available frame instead of seeking back
+    to I-frame. So, incase of Mpeg2TS/Mpeg2PS clips we use hardware decoder
+    for thumbnail generation*/
+    if ((!strcasecmp(filemime, MEDIA_MIMETYPE_CONTAINER_MPEG2TS))
+        ||(!strcasecmp(filemime, MEDIA_MIMETYPE_CONTAINER_MPEG2PS))){
+            useHWCodec = 1;
+    }
+    if (!useHWCodec){
+        LOGV("Trying with s/w codec(NonMpeg2TS/PS clip)");
+        frame =  extractVideoFrameWithCodecFlags(
+           &mClient, trackMeta, source, OMXCodec::kPreferSoftwareCodecs,
+           timeUs, option);
+    }
+    if (frame == NULL){
+       LOGV("Software decoder failed to extract thumbnail, "
+           "trying hardware decoder.");
+       frame = extractVideoFrameWithCodecFlags(&mClient, trackMeta, source, 0,
+                     timeUs, option);
+    }
 #else
+
     VideoFrame *frame =
         extractVideoFrameWithCodecFlags(
 #endif
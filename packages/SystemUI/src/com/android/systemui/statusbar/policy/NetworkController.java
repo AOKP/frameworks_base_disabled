68d9d4982d64cdc0a4dd674d3ad689ea8c7ec004
LorDClockaN <davor@losinj.com>
2012-11-06 18:24:45 +0100

    Revert "Telephony: Do not display the data icon if data is not connected."
    This reverts commit f861a297ca32f4f5cc5e6837dce1f3975be756f7.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -1131,10 +1131,7 @@ public class NetworkController extends BroadcastReceiver {
                 mHasMobileDataFeature ? mDataSignalIconId : mWifiIconId;
             mContentDescriptionCombinedSignal = mHasMobileDataFeature
                 ? mContentDescriptionDataType : mContentDescriptionWifi;
-        }
 
-        if (!mDataConnected) {
-            Slog.d(TAG, "refreshViews: Data not connected!! Set no data type icon / Roaming");
             mDataTypeIconId = 0;
             if (isCdma()) {
                 if (isCdmaEri()) {
